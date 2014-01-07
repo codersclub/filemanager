948c5f873824053ae0198f3448ff53683946ced3
vot <vvotintsev@gmail.com>
2014-01-07 11:12:17 +0300

    New language added: ru (Russian)

---
+++ lang/ru.php
@@ -0,0 +1,29 @@
+<?php
+// Russian Language File for FileManager
+
+$labels = [
+	'level_up' => 'Наверх',
+	'dir_not_writable' => 'Папка закрыта от записи',
+	'file_not_writable' => 'Ошибка создания файла',
+	'create_new_directory' => 'Создание новой папки',
+	'dir_name' => 'Название папки',
+	'upload_file' => 'Загрузить новый файл',
+	'delete_file' => 'Удалить файл',
+	'file_not_deletable' => 'Ошибка удаления файла: ',
+	'get_filepath' => 'Показать путь к файду',
+	'really_delete' => 'Вы уверены, что хотите удалить этот файл?',
+	'file_type_not_allowed' => 'Данный Mime-Type запрещён к загрузке',
+	'actual_path' => 'Вы здесь',
+	'dir_not_found' => 'Папка не найдена',
+        'file_name' => 'Имя',
+        'file_date' => 'Дата',
+        'file_size' => 'Размер',
+        'action' => 'Действие',
+        'home_page' => 'Главная',
+        'create_dir_page' => 'Создать папку',
+        'upload_page' => 'Загрузка',
+        'other_page' => 'На будущее...',
+        'root_dir' => 'Корень',
+	'upload' => 'Загрузить',
+	'for_a_future' => 'Небольшая заготовка на будущее',
+];
